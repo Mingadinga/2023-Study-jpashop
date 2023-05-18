130da6a8e220adec65d35e58d4e3f8d490cb760b
mingadinga <minpearl0826@gmail.com>
2023-05-18 12:28:28 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext // 스프링부트가 주입
+    private EntityManager em;
+
+    // 커맨드와 쿼리를 분리하라 - id 정도만 반환
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
+}
---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test @Transactional // em을 통한 모든 데이터 변경은 트랜잭션 안에서 이루어져야함, 자동으로 롤백
+    @Rollback(value = false)
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("A");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member); // 같은 영속성 컨텍스트 안에서는 id가 같으면 같은 엔티티로 인식. 1차 캐시에서 찾아옴.
+    }
+
+}
