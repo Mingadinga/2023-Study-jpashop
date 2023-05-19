54a5487599041c08b1239cb5e3ba5504ea83d806
mingadinga <minpearl0826@gmail.com>
2023-05-19 13:18:36 +0900

    2. 도메인 분석과 엔티티 클래스 개발

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable // 값 타입
+@Getter
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+
+    protected Address() { // 불변 객체로 설계해야함
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "order_id") // 양방향 참조 시, 연관관계의 주인은 order_id
+    private List<Order> orders = new ArrayList<>();
+
+}
